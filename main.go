6dea37c1bee3b5a1289653d3c87ef8b489d0d3db
mike-bionic <muhammedjepbarov@gmail.com>
2021-07-23 00:01:07 +0500

    updated routes

--- main.go
+++ main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
 	_ "github.com/lib/pq"
 )
 
+// func query_handler() {
+
+// 	query_string := "SELECT \"ResId\", \"ResName\" FROM tbl_dk_resource"
+// 	query_string = "SELECT * FROM tbl_dk_res_category"
+// 	do_db_query(query_string)
+
+// }
+
+type server struct{}
+
+func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message": "hello world"}`))
+}
+
 func main() {
-
-	query_string := "SELECT \"ResId\", \"ResName\" FROM tbl_dk_resource"
-	query_string = "SELECT * FROM tbl_dk_res_category"
-	do_db_query(query_string)
-
-	// fmt.Println(response)
-
+	s := &server{}
+	http.Handle("/", s)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
