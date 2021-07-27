881a4c5048de6ec50a92ff6ce7d4d869ab38c15c
mike-bionic <muhammedjepbarov@gmail.com>
2021-07-27 18:11:12 +0500

    completed config and env setup

--- go.mod
+++ go.mod
@@ -5,5 +5,8 @@ go 1.16
 require (
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
 	github.com/lib/pq v1.10.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,11 +1,67 @@
 package main
 
-func main() {
-	db_type := "postgres"
-	db_user := "postgres"
-	db_password := "123456"
-	db_host := "localhost"
-	db_database := "dbSapHasap"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
+)
+
+type config struct {
+	ListenAddress string `json:"listen_address"`
+	DB_Type       string `json:"db_type"`
+	DB_User       string `json:"db_user"`
+	DB_Password   string `json:"db_password"`
+	DB_Host       string `json:"db_host"`
+	DB_Database   string `json:"db_database"`
+}
+
+func ReadConfig(source string) (c *config, err error) {
+	var raw []byte
+	raw, err = ioutil.ReadFile(source)
+	if err != nil {
+		eMsg := "error reading config from file"
+		log.WithError(err).Error(eMsg)
+		err = errors.Wrap(err, eMsg)
+		return
+	}
+	err = json.Unmarshal(raw, &c)
+	if err != nil {
+		eMsg := "error parsing config from json"
+		log.WithError(err).Error(eMsg)
+		err = errors.Wrap(err, eMsg)
+		c = nil
+	}
+	return
+}
+
+func run() error {
+	var configFile string
+	var conf *config
+	var err error
+	err = godotenv.Load()
+	if err != nil {
+		log.WithError(err).Error("error loading .env, ignoring")
+	}
+	configFile = os.Getenv("GATEWAYGO_CONFIG_FILE")
+	if configFile == "" {
+		configFile = "gatewaygo.config.json"
+	}
+	conf, err = ReadConfig(configFile)
+	if err != nil {
+		log.WithError(err).WithField("config-file", configFile).Error("error loading configuration")
+		return err
+	}
+
+	db_type := conf.DB_Type
+	db_user := conf.DB_User
+	db_password := conf.DB_Password
+	db_host := conf.DB_Host
+	db_database := conf.DB_Database
 
 	a := App{}
 	a.Initialize(
@@ -16,6 +72,14 @@ func main() {
 		db_database,
 	)
 
-	a.Run(":8080")
+	a.Run(conf.ListenAddress)
 
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "%v", err)
+		os.Exit(1)
+	}
 }
